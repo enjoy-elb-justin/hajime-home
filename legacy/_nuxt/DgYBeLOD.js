907537db85d20c9b5bb04f09cc23c6c23f45e4dd
Justin Chen <interjc@live.com>
2026-09-04 11:52:35 +0900

    add legacy web

---
+++ legacy/_nuxt/DgYBeLOD.js
@@ -0,0 +1,1 @@
+import{ut as e}from"./DiFoSUVA.js";import{xt as t}from"./C7hSKfIO.js";function n(){let n=e(!1);return t()||(`ontouchstart`in window||navigator.maxTouchPoints>0||window.matchMedia(`(pointer:coarse)`).matches)&&(n.value=!0),{isTouchDevice:n}}export{n as t};
